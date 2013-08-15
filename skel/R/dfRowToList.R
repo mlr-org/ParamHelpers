015b32d59941505daa8fa7b1ca0e015352a8202e
Bernd Bischl bischl@Bischl <bernd_bischl@gmx.net>
2013-08-15 15:02:46 +0200

    add extra option to dfRowsToList to filter NAs

--- skel/R/dfRowToList.R
+++ skel/R/dfRowToList.R
@@ -1,5 +1,4 @@
-#' Internal function to convert a data.frame row to
-#' a parameter-value- / list-represenation.
+#' Convert a data.frame row to list of parameter-value-lists.
 #'
 #' @param df [\code{data.frame}]\cr
 #'   Data.frame, probably from OptPatDF.
@@ -10,15 +9,15 @@
 #'   Row index.
 #' @return [\code{list}]. Named by parameter ids. 
 #' @export
-dfRowToList = function(df, par.set, i) {
+dfRowToList = function(df, par.set, i, remove.missing.values=FALSE) {
   for (j in 1:ncol(df)) {
     if (is.factor(df[,j]))
       df[,j] = as.character(df[,j])
   }
-  dfRowToList2(df, par.set, i)
+  dfRowToList2(df, par.set, i, remove.missing.values)
 }
 
-dfRowToList2 = function(df, par.set, i) {
+dfRowToList2 = function(df, par.set, i, remove.missing.values=FALSE) {
   df = df[i,,drop=FALSE]
   pars = par.set$pars
   col = 0
@@ -29,26 +28,36 @@ dfRowToList2 = function(df, par.set, i) {
     if (p$type %in% c("numericvector", "integervector", "discretevector", "logicalvector")) 
       col = (cc + 1) : (cc + p$len)   
     else 
-      col = cc + 1    
-    # convert from to df to vector and remove names in the next 2 
-    if (p$type %in% c("numeric", "numericvector")) 
-      x[[p$id]] = as.numeric(df[,col])
-    else if (p$type %in% c("integer", "integervector")) 
-      x[[p$id]] = as.integer(df[,col])
-    else if (p$type %in% c("logical", "logicalvector"))
-      x[[p$id]] = as.logical(df[,col])
-    else if (p$type %in% c("discrete", "discretevector"))
-      x[[p$id]] = discreteNameToValue(p, as.character(df[,col]))
+      col = cc + 1  
+    entry = df[,col]
+    if (all(is.na(entry))) {
+      # missing value due to unsatified dependencies / requires
+      x[[p$id]] = NA
+    } else {
+      # convert from to df to vector and remove names in the next 2 
+      if (p$type %in% c("numeric", "numericvector")) 
+        x[[p$id]] = as.numeric(entry)
+      else if (p$type %in% c("integer", "integervector")) 
+        x[[p$id]] = as.integer(entry)
+      else if (p$type %in% c("logical", "logicalvector"))
+        x[[p$id]] = as.logical(entry)
+      else if (p$type %in% c("discrete", "discretevector"))
+        x[[p$id]] = discreteNameToValue(p, as.character(entry))
+    }
+  }
+  if (remove.missing.values) {
+    j = sapply(x, isScalarNA)
+    x[j] = NULL
   }
   return(x)
 }
 
 #' @export
 #' @rdname dfRowToList
-dfRowsToList = function(df, par.set) {
+dfRowsToList = function(df, par.set, remove.missing.values=FALSE) {
   for (j in 1:ncol(df)) {
     if (is.factor(df[,j]))
       df[,j] = as.character(df[,j])
   }
-  lapply(1:nrow(df), dfRowToList2, df=df, par.set=par.set)
+  lapply(1:nrow(df), dfRowToList2, df=df, par.set=par.set, remove.missing.values=remove.missing.values)
 }
---
+++ skel/inst/tests/test_dfRowToList.R
@@ -0,0 +1,50 @@
+context("dfRowToList")
+
+
+test_that("dfRowToList", {
+  ps = makeParamSet(
+    makeDiscreteParam("x", values = list(a=iris, b=123)),
+    makeNumericParam("y", lower=1, upper=2),
+    makeNumericVectorParam("z", len=2, lower=10, upper=20)
+  )
+  des = generateDesign(10, par.set=ps)
+  vals = dfRowsToList(des, ps)
+  expect_true(is.list(vals) && length(vals) == nrow(des))
+  for (i in seq_row(des)) {
+    v = vals[[i]]
+    expect_true(is.list(v) && length(v) == 3 && names(v) == c("x", "y", "z"))
+    expect_true(identical(v$x, iris) || identical(v$x, 123))
+    expect_true(is.numeric(v$y) && length(v$y) == 1 && v$y >= 1 && v$y <= 2)
+    expect_true(is.numeric(v$z) && length(v$z) == 2 && all(v$z >= 10 & v$z <= 20))
+  }
+})
+
+test_that("requires works", {
+  ps = makeParamSet(
+    makeDiscreteParam("x", values = c("a", "b")),
+    makeNumericParam("y", lower=1, upper=2, requires = quote(x == "a")),
+    makeNumericVectorParam("z", len=2, lower=10, upper=20, requires = quote(x == "b"))
+  )
+  des = generateDesign(10, par.set=ps)
+  
+  mycheck = function(vals, rm) {
+    expect_true(is.list(vals) && length(vals) == nrow(des))
+    for (i in seq_row(des)) {
+      v = vals[[i]]
+      len = ifelse(rm, 2, 3)
+      expect_true(is.list(v) && length(v) == len && (names(v) %in% c("x", "y", "z")))
+      expect_true(is.character(v$x) && length(v$x) == 1 && v$x %in% c("a", "b"))
+      if (v$x == "a") {
+        expect_true(is.numeric(v$y) && length(v$y) == 1 && v$y >= 1 && v$y <= 2)
+        if (rm) expect_true(is.null(v$z)) else expect_true(length(v$z) == 1 && is.na(v$z))
+      } else if (v$x == "b") {
+        if (rm) expect_true(is.null(v$y)) else expect_true(length(v$y) == 1 && is.na(v$y))
+        expect_true(is.numeric(v$z) && length(v$z) == 2 && all(v$z >= 10 & v$z <= 20))
+      }
+    }    
+  }
+  vals = dfRowsToList(des, ps, remove.missing.values=TRUE)
+  mycheck(vals, TRUE)
+  vals = dfRowsToList(des, ps, remove.missing.values=FALSE)
+  mycheck(vals, FALSE)
+})
