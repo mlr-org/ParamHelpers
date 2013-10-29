e77d831c1e0cd2eca325c974c063f161d6ab842d
Bernd Bischl bischl@Bischl <bernd_bischl@gmx.net>
2013-10-29 18:33:22 +0100

    more work on dfRowsToList in C

--- src/c_dfRowsToList.c
+++ src/c_dfRowsToList.c
@@ -12,7 +12,7 @@ SEXP c_dfRowsToList(SEXP s_df, SEXP s_pars, SEXP s_types, SEXP s_parnames, SEXP 
   int parlen; /* length of param we are currently handling */
   int colcount = 0; /* when we iterate params, what is the (first) column of s_df that corresponds? */
   SEXP s_res, s_rowlist, s_parval, s_call;
-  int all_missing;
+  Rboolean all_missing;
 
   /* we iterate thru rows then params. */
   s_res = PROTECT(NEW_LIST(nrow_df));
@@ -24,32 +24,44 @@ SEXP c_dfRowsToList(SEXP s_df, SEXP s_pars, SEXP s_types, SEXP s_parnames, SEXP 
     for (par = 0; par < npars; par++) { /* iter thru params */
       parlen = lens[par];
       type = types[colcount];
+      all_missing = TRUE;
       /* copy vector-param block of row to s_parval */
       if (type == 1) { /* numerics */
         s_parval = PROTECT(NEW_NUMERIC(parlen));
-        for (k = 0; k < parlen; k++)
+        for (k = 0; k < parlen; k++) {
           REAL(s_parval)[k] = REAL(VECTOR_ELT(s_df, colcount+k))[row];
-      } else if (type == 2 || type == 4) { /* integers, logical */
+          if (!ISNAN(REAL(s_parval)[k]))
+            all_missing = FALSE;
+        }
+      } else if (type == 2) { /* integers */
         s_parval = PROTECT(NEW_INTEGER(parlen));
-        for (k = 0; k < parlen; k++)
+        for (k = 0; k < parlen; k++) {
           INTEGER(s_parval)[k] = INTEGER(VECTOR_ELT(s_df, colcount+k))[row];
-      } else { /* factors */
+          if (INTEGER(s_parval)[k] == NA_INTEGER)
+            all_missing = FALSE;
+        }
+      } else if (type == 3) { /* factors */
         s_parval = PROTECT(NEW_CHARACTER(parlen));
-        for (k = 0; k < parlen; k++)
+        for (k = 0; k < parlen; k++) {
           SET_STRING_ELT(s_parval, k, STRING_ELT(VECTOR_ELT(s_df, colcount+k), row));
+          if (STRING_ELT(s_parval, k) == NA_STRING)
+            all_missing = FALSE;
+        }
+      } else if (type == 4) { /* logical */
+        s_parval = PROTECT(NEW_LOGICAL(parlen));
+        for (k = 0; k < parlen; k++) {
+          LOGICAL(s_parval)[k] = LOGICAL(VECTOR_ELT(s_df, colcount+k))[row];
+          if (LOGICAL(s_parval)[k] == NA_LOGICAL)
+            all_missing = FALSE;
+        }
       }
       
       /* are all entries in s_parval NA ? */
-      all_missing = 1;
-      for (k = 0; k < parlen; k++) { 
-        if(!MISSING(s_parval[k]))
-          all_missing = 0;
-      }
       if (all_missing) 
-        s_parval = NA_LOGICAL;
+        s_parval = ScalarLogical(NA_LOGICAL);
 
       /* convert discrete names to values */ 
-      if (!all_missing && type == 4) {
+      if (!all_missing && type == 3) {
         SETCADR(s_call, VECTOR_ELT(s_pars, par));
         SETCADDR(s_call, s_parval);
         s_parval = PROTECT(eval(s_call, R_GlobalEnv)); 
