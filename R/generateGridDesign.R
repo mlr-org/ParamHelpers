54eb6f977a1f6284f3871c4868ce2789d1b4fbed
Bernd Bischl <you@example.com>
2014-04-16 05:27:05 +0200

    improve generateGridDesign, NOT DONE

--- R/generateGridDesign.R
+++ R/generateGridDesign.R
@@ -2,47 +2,105 @@
 
 #' Generates a grid design for a parameter set.
 #'
-#' @param n [\code{integer(k)}]\cr
-#'   Resolution of the grid for each ot the \code{k} parameters in the \code{par.set}.
+#' @param n [\code{integer}]\cr
+#'   Resolution of the grid for each numeric/integer parameter in \code{par.set}.
+#'   For vector parameters, n is the resolution per dimension.
+#'   Either pass one resolution for all parameters, or a named vector.
 #' @param par.set [\code{\link{ParamSet}}]\cr
 #'   Parameter set.
+#' @param ints.as.num [\code{logical(1)}]\cr
+#'   Should parameters of type \dQuote{integer} or \dQuote{integervector} generate numeric columns?
+#'   Default is \code{FALSE}.
+#' @param discretes.as.factor [\code{logical(1)}]\cr
+#'   Should discrete parameters have columns of type \dQuote{factor} in result?
+#'   Otherwise character columns are generated.
+#'   Default is \code{TRUE}.
+#' @param logicals.as.factor [\code{logical(1)}]\cr
+#'   Should logical parameters have columns of type \dQuote{factor} in result?
+#'   Otherwise logical columns are generated.
+#'   Default is \code{FALSE}.
 #' @param trafo [\code{logical(1)}]\cr
 #'   Transform all parameters by using theirs respective transformation functions.
 #'   Default is \code{FALSE}.
-#' @return The created design is a data.frame. Columns are named by the ids of the parameters.
+#' @return [\code{data.frame.}]. Columns are named by the ids of the parameters.
 #'   If the \code{par.set} argument contains a vector parameter, its corresponding column names
 #'   The result will have an \code{logical(1)} attribute \dQuote{trafo},
 #'   which is set to the value of argument \code{trafo}.
 #' @export
 #' @examples
-#' ps <- makeParamSet(
+#' ps = makeParamSet(
 #'   makeNumericParam("x1", lower=-2, upper=1),
 #'   makeIntegerParam("x2", lower=10, upper=20, trafo = function(x) x^(-2))
 #' )
 #' # simple gridDesign
 #' generateGridDesign(c(4,5), ps, trafo = TRUE)
-generateGridDesign = function(n=10L, par.set, trafo=FALSE) {
-  n = convertIntegers(n)
-  checkArg(n, "integer", na.ok=FALSE)
+generateGridDesign = function(par.set, resolution, trafo = FALSE, ints.as.num = FALSE,
+  discretes.as.factor = TRUE, logicals.as.factor = FALSE) {
+
   checkArg(par.set, "ParamSet")
-  checkArg(trafo, "logical", len=1L, na.ok=FALSE)
-  
-  if (length(par.set$pars) == 0)
-    stop("par.set must not be empty!")
-  if(any(sapply(par.set$pars, function(x) is(x, "LearnerParameter"))))
-    stop("No par.set parameter in 'generateDesign' can be of class 'LearnerParameter'! Use basic parameters instead to describe you region of interest!")
   ids = getParamIds(par.set)
   pars = par.set$pars
-  if(length(n) == 1)
-    n = rep(n, length(pars))
-  if(is.null(names(n)))
-    names(n) = ids 
-  disc.pars = lapply(ids, function(id) {
-    discretizeParam(pars[[id]], length.out=n[id], trafo=trafo)
-  })
-  disc.values = lapply(disc.pars, function(x) unlist(x$values))
-  res = expand.grid(disc.values)
-  colnames(res) = ids
+  n = length(pars)
+  if (n == 0L)
+    stop("par.set must not be empty!")
+  if(any(sapply(pars, function(x) inherits(x, "LearnerParameter"))))
+    stop("No par.set parameter in 'generateDesign' can be of class 'LearnerParameter'! Use basic parameters instead to describe you region of interest!")
+
+  m = sum(getParamLengths(par.set))
+  pids1 = getParamIds(par.set)  
+  pids2 = getParamIds(par.set, repeated = TRUE, with.nr = TRUE)  
+  
+  resolution = convertIntegers(resolution)
+  print(str(resolution))
+  if (length(resolution) == 1L) {
+    resolution = setNames(rep(resolution, n), pids1)
+  }
+  print(str(resolution))
+  checkArg(resolution, "integer", na.ok = FALSE, lower = 1L, len = n)
+  if (!isProperlyNamed(resolution) || !all(names(resolution) == pids1))
+    stop("'resolution' must be named with parameter ids!")
+  resolution = resolution[pids1]
+
+  checkArg(trafo, "logical", len = 1L, na.ok = FALSE)
+
+  vals.list = setNames(vector("list", m), pids2)
+  el.counter = 1L
+  for (i in 1:n) {
+    p = pars[[i]]
+    type = p$type
+    reso = resolution[[i]]
+    if (isNumeric(p)) {
+      lower = p$lower
+      upper = p$upper
+    }
+    if (isDiscrete(p, include.logical = FALSE)) {
+     discvals = p$values
+    }
+    for (j in 1:p$len) {
+      if (isDiscrete(p, include.logical = FALSE)) {
+        newvals = names(discvals)
+      } else if (isLogical(p)) {
+        newvals = if (logicals.as.factor) 
+          factor(c("TRUE", "FALSE"), levels = c("TRUE", "FALSE")) 
+        else 
+          c(TRUE, FALSE)
+      } else if (isNumeric(p, include.int = TRUE)) {
+        newvals = seq(from = lower[[j]], to = upper[[j]], length.out = reso)
+        # round for integer
+        if (isInteger(p)) { 
+          newvals = as.integer(unique(round(newvals)))
+          if (ints.as.num)
+            newvals = as.numeric(newvals)
+        }
+      } else {
+        stopf("generateGridDesign cannot be used for param '%s' of type '%s'!", p$id, p$type)
+      }
+      vals.list[[el.counter]] = newvals
+      el.counter = el.counter + 1
+    }
+  }
+  res = expand.grid(vals.list, KEEP.OUT.ATTRS = FALSE)
+  colnames(res) = pids2
   attr(res, "trafo") = trafo
   res
 }
--- inst/tests/test_generateGridDesign.R
+++ inst/tests/test_generateGridDesign.R
@@ -1,21 +1,86 @@
 context("generateGridDesign")
 
-
-test_that("simple num/int/bool grid design", {
-  ps2 = makeParamSet(
-    makeNumericParam("x1", lower=-2, upper=1),
-    makeIntegerParam("x2", lower=10, upper=20, trafo=sqrt),
-    makeLogicalParam("x3")
+test_that("generateGridDesign", {
+  ps = makeParamSet(
+    makeNumericParam("x", lower = 1, upper = 5),
+    makeIntegerParam("y", lower = 2, upper= 6)
   )
-  des = generateGridDesign(n = c(4, 2, 10), par.set=ps2)
-  expect_equal(nrow(des), 16)
-  expect_equal(ncol(des), 3)
-  expect_true(is.numeric(des[,1]))
-  expect_true(is.numeric(des[,2]))
-  expect_true(is.logical(des[,3]))
-  expect_true(is.integer(des[,2]))
-  expect_equal(colnames(des), getParamIds(ps2))
-  des = generateGridDesign(n = c(2,5,2), par.set=ps2, trafo=TRUE)
-  expect_true(is.numeric(des[,2]))
-  expect_true(length(setdiff(des[,2], round(sqrt(seq(from=10, to=20, length.out=5))))) == 0)
+  d = generateGridDesign(ps, resolution = 3L, ints.as.num = FALSE)
+  e = expand.grid(x = c(1, 3, 5), y = c(2L, 4L, 6L), KEEP.OUT.ATTRS = FALSE)
+  attr(e, "trafo") = FALSE
+  expect_equal(d, e)
+  d = generateGridDesign(ps, resolution = 3L, ints.as.num = TRUE)
+  e = expand.grid(x = c(1, 3, 5), y = c(2, 4, 6), KEEP.OUT.ATTRS = FALSE)
+  attr(e, "trafo") = FALSE
+  expect_equal(d, e)
+
+  ps = makeParamSet(
+    makeNumericParam("x", lower = 1, upper = 5),
+    makeIntegerParam("y", lower = 2, upper= 6),
+    makeLogicalParam("z")
+  )
+  d = generateGridDesign(ps, resolution = 3L, logicals.as.factor = FALSE)
+  e = expand.grid(x = c(1, 3, 5), y = c(2L, 4L, 6L), z = c(TRUE, FALSE), KEEP.OUT.ATTRS = FALSE)
+  attr(e, "trafo") = FALSE
+  expect_equal(d, e)
+  d = generateGridDesign(ps, resolution = 3L, logicals.as.factor = TRUE)
+  e = expand.grid(x = c(1, 3, 5), y = c(2L, 4L, 6L), z = c("TRUE", "FALSE"), KEEP.OUT.ATTRS = FALSE)
+  attr(e, "trafo") = FALSE
+  expect_equal(d, e)
+
+  # vectors
+  ps = makeParamSet(
+    makeNumericVectorParam("x", len = 2L, lower = 1, upper = 2),
+    makeIntegerVectorParam("y", len = 2L, lower = 3, upper = 4),
+    makeLogicalVectorParam("z", len = 2L)
+  )
+  d = generateGridDesign(ps, resolution = 2L)
+  e = expand.grid(
+    x1 = c(1, 2), x2 = c(1, 2),
+    y1 = c(3L, 4L), y2 = c(3L, 4L),
+    z1 = c(TRUE, FALSE), z2 = c(TRUE, FALSE),
+    KEEP.OUT.ATTRS = FALSE
+  )
+  attr(e, "trafo") = FALSE
+  expect_equal(d, e)
+  ps = makeParamSet(
+    makeNumericVectorParam("x", len = 2L, lower = 1, upper = 2),
+    makeIntegerVectorParam("y", len = 2L, lower = 3, upper = 4),
+    makeLogicalVectorParam("z", len = 2L)
+  )
+  d = generateGridDesign(ps, resolution = 2L, logicals.as.factor = TRUE, ints.as.num = TRUE)
+  e = expand.grid(
+    x1 = c(1, 2), x2 = c(1, 2),
+    y1 = c(3, 4), y2 = c(3, 4),
+    z1 = c("TRUE", "FALSE"), z2 = c("TRUE", "FALSE"),
+    KEEP.OUT.ATTRS = FALSE
+  )
+  attr(e, "trafo") = FALSE
+  expect_equal(d, e)
+
+  # trafo
+  ps = makeParamSet(
+    makeNumericParam("x", lower = 0, upper = 1),
+    makeNumericParam("y", lower = 3, upper = 4, trafo = function(x) 2*x)
+  )
+  d = generateGridDesign(ps, resolution = c(x = 2, y = 4), trafo = TRUE)
+  e = expand.grid(
+    x = seq(0, 1, length.out = 2),
+    y = sqrt(seq(3, 4, length.out = 4)),
+    KEEP.OUT.ATTRS = FALSE
+  )
+  attr(e, "trafo") = TRUE
+  expect_equal(d, e)
+  ps = makeParamSet(
+    makeNumericVectorParam("x", len = 2, lower = 0, upper = 1, trafo = function(x) x / sum(x))
+  )
+  d = generateGridDesign(ps, resolution = 4, trafo = TRUE)
+  e = expand.grid(
+    x1 = seq(0, 1, length.out = 4),
+    x2 = seq(0, 1, length.out = 4),
+    KEEP.OUT.ATTRS = FALSE
+  )
+  e = rowApply(e, function(x) x / sum(x))
+  attr(e, "trafo") = TRUE
+  expect_equal(d, e)
 })
