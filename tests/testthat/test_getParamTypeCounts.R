1aae1334b68b2f59aa05f21dce38c97b71c014de
Jakob Bossek <info@jakobbossek.de>
2014-04-24 22:26:23 +0200

    add test for getParamTypeCounts

---
+++ tests/testthat/test_getParamTypeCounts.R
@@ -0,0 +1,18 @@
+context("getParamTypeCounts")
+
+test_that("getParamTypeCounts", {
+  par.set = makeParamSet(
+    makeNumericParam("numeric1", lower = 0L, upper = 10L),
+    makeIntegerParam("integer1", lower = 0L, upper = 5L),
+    makeIntegerParam("integer2", lower = 0L, upper = 5L),
+    makeDiscreteParam("discrete1", values = letters[1:5])
+  )
+
+  or = getParamTypeCounts(par.set)
+  expect_equal(or$numeric, 1L)
+  expect_equal(or$integer, 2L)
+  expect_equal(or$discrete, 1L)
+  sapply(setdiff(getSupportedParamTypes(), getTypes(par.set)), function(type) {
+    expect_equal(or[[type]], 0L)
+  })
+})
