633d6128f3a451e2fa5dea1543f66db0f4b89aef
Bernd Bischl <you@example.com>
2014-05-20 20:33:44 +0200

    clean up

--- R/OptPathDF.R
+++ R/OptPathDF.R
@@ -1,13 +1,13 @@
 #' @rdname OptPath
 #' @aliases OptPathDF
 #' @export
-makeOptPathDF = function(par.set, y.names, minimize, add.transformed.x=FALSE) {
+makeOptPathDF = function(par.set, y.names, minimize, add.transformed.x = FALSE) {
   checkArg(par.set, "ParamSet")
-  checkArg(y.names, "character", na.ok=FALSE)
-  checkArg(minimize, "logical", na.ok=FALSE)
+  checkArg(y.names, "character", na.ok = FALSE)
+  checkArg(minimize, "logical", na.ok = FALSE)
   obj = makeOptPath(par.set, y.names, minimize, add.transformed.x)
-  ns = c(getParamIds(par.set, repeated=TRUE, with.nr=TRUE), y.names)
-  obj$env$path = as.data.frame(matrix(0, nrow=0, ncol=length(ns)))
+  ns = c(getParamIds(par.set, repeated = TRUE, with.nr = TRUE), y.names)
+  obj$env$path = as.data.frame(matrix(0, nrow = 0, ncol = length(ns)))
   colnames(obj$env$path) = ns
   class(obj) = c("OptPathDF", class(obj))
   return(obj)
@@ -25,8 +25,8 @@ as.data.frame.OptPathDF = function(x, row.names = NULL, optional = FALSE,
   df = x$env$path
   df = cbind(df, dob = x$env$dob, eol = x$env$eol)
   df = convertDataFrameCols(df, chars.as.factor = discretes.as.factor)
-  ## backward compatibility with "old" OptPath objects
-  if(!is.null(x$env$error.message))
+  # backward compatibility with "old" OptPath objects
+  if (!is.null(x$env$error.message))
     df = cbind(df, error.message = x$env$error.message, stringsAsFactors = FALSE)
   return(df)
 }
@@ -40,30 +40,30 @@ getOptPathEl.OptPathDF = function(op, index) {
     stop("Index must be between 1 and ", n, "!")
   e = op$env
   path = e$path
-  y = unlist(path[index, op$y.names, drop=FALSE])
+  y = unlist(path[index, op$y.names, drop = FALSE])
   # remove y names from path, only consider x
-  path = path[, setdiff(colnames(path), op$y.names), drop=FALSE]
+  path = path[, setdiff(colnames(path), op$y.names), drop = FALSE]
   x = dfRowToList(path, op$par.set, index)
-  ## backward compatibility with "old" OptPath objects
-  if(is.null(e$error.message))
-    list(x=x, y=y, dob=e$dob[index], eol=e$eol[index])
+  # backward compatibility with "old" OptPath objects
+  if (is.null(e$error.message))
+    list(x = x, y = y, dob = e$dob[index], eol = e$eol[index])
   else
-    list(x=x, y=y, dob=e$dob[index], eol=e$eol[index], error.message = e$error.message[index])
+    list(x = x, y = y, dob = e$dob[index], eol = e$eol[index], error.message = e$error.message[index])
 }
 
 #' @export
-addOptPathEl.OptPathDF = function(op, x, y, dob=getOptPathLength(op)+1L, eol=as.integer(NA),
-  error.message = NA_character_, check.feasible=!op$add.transformed.x) {
+addOptPathEl.OptPathDF = function(op, x, y, dob = getOptPathLength(op)+1L, eol = as.integer(NA),
+  error.message = NA_character_, check.feasible = !op$add.transformed.x) {
 
-  checkArg(x, "list", len=length(op$par.set$pars))
-  checkArg(y, "numeric", len=length(op$y.names))
+  checkArg(x, "list", len = length(op$par.set$pars))
+  checkArg(y, "numeric", len = length(op$y.names))
   dob = convertInteger(dob)
   checkArg(dob, "integer", 1)
   eol = convertInteger(eol)
   checkArg(eol, "integer", 1)
   checkArg(error.message, "character", 1)
   if (check.feasible) {
-    if(!isFeasible(op$par.set, x))
+    if (!isFeasible(op$par.set, x))
       stop("Trying to add infeasible x values to opt path: ", convertToShortString(x))
   }
 
@@ -85,8 +85,8 @@ addOptPathEl.OptPathDF = function(op, x, y, dob=getOptPathLength(op)+1L, eol=as.
   k = length(op$env$dob) + 1
   op$env$dob[k] = dob
   op$env$eol[k] = eol
-  ## backward compatibility with "old" OptPath objects
-  if(is.null(op$env$error.message))
+  # backward compatibility with "old" OptPath objects
+  if (is.null(op$env$error.message))
     op$env$error.message[1:(k-1)] = NA_character_
   op$env$error.message[k] = error.message
   invisible(NULL)
@@ -94,13 +94,13 @@ addOptPathEl.OptPathDF = function(op, x, y, dob=getOptPathLength(op)+1L, eol=as.
 
 
 #' @export
-getOptPathY.OptPathDF = function(op, names, drop=TRUE) {
+getOptPathY.OptPathDF = function(op, names, drop = TRUE) {
   if (missing(names))
     names = op$y.names
   else
-    checkArg(names, subset=op$y.names)
-  checkArg(drop, "logical", len=1L, na.ok=FALSE)
-  y = as.matrix(op$env$path[, names, drop=FALSE])
+    checkArg(names, subset = op$y.names)
+  checkArg(drop, "logical", len = 1L, na.ok = FALSE)
+  y = as.matrix(op$env$path[, names, drop = FALSE])
   if (drop && length(names) == 1L)
     y = as.numeric(y)
   return(y)
