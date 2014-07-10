174ae82d275c07a6102f1968359207437f058eff
Bernd Bischl <bernd_bischl@gmx.net>
2014-07-10 19:45:36 +0200

    R docs for as.data.frame

--- R/OptPathDF_as_data_frame.R
+++ R/OptPathDF_as_data_frame.R
@@ -1,4 +1,22 @@
 #' Convert optimization path to data.frame.
+
+#' @description
+#' The following types of columns are created:
+#' \tabular{ll}{
+#'  x-numeric(vector)   \tab  \code{numeric}  \cr
+#'  x-integer(vector)   \tab  \code{integer}  \cr
+#'  x-discrete(vector)  \tab  \code{factor} (names of values = levels) \cr
+#'  x-logical(vector)   \tab  \code{logical} \cr
+#'  y-columns           \tab  \code{numeric}  \cr
+#'  dob                 \tab  \code{integer}  \cr
+#'  eol                 \tab  \code{integer}  \cr
+#'  error.message       \tab  \code{character} \cr
+#'  exec.time           \tab  \code{numeric}  \cr
+#'  extra-columns       \tab  any \cr
+#' }
+#' If you want to convert these, look at \code{\link[BBmisc]{convertDataFrameCols}}.
+#' Dependent parameters whose constraints are unsatisfied generate \code{NA} entries in their
+#' respective columns.
 #'
 #' @param x [\code{\link{OptPath}}]\cr
 #'   Optimization path.
