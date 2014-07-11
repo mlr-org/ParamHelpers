d83574baa9993b7e2a7d3eea206d9a8f46f824a5
Bernd Bischl <bernd_bischl@gmx.net>
2014-07-11 22:33:33 +0200

    clean up repairPoint

--- R/repairPoint.R
+++ R/repairPoint.R
@@ -1,12 +1,14 @@
-#' Repairs points outside of box constraints by clipping it to bounds.
+#' Repairs values of numeric and integer parameters out side of constraints.
 #'
-#' @param par.set [\code{\link{ParamSet}}]\cr
-#'   Parameter set.
+#' Clips values outside of box constraints to bounds.
+#'
+#' @template arg_parset
 #' @param x [\code{list}]\cr
-#'   List of values which eventually are located outside of the bounds.
+#'   List of parameter values. Must be in correct order.
+#'   Values corresponding to non-numeric/integer types are left unchanged.
 #' @param warn [\code{logical(1)}]\cr
-#'   Boolean indicating whether a warning should be printed each time 
-#'   a value is being repaired. Default is \code{FALSE}.
+#'   Boolean indicating whether a warning should be printed each time a value is repaired.
+#'   Default is \code{FALSE}.
 #' @return [\code{list}]:
 #'   List of repaired points.
 #' @export
@@ -15,10 +17,11 @@ repairPoint = function(par.set, x, warn = FALSE) {
   assertList(x)
   assertFlag(warn)
   Map(function(par, val) {
-    if (isNumeric(par)) {
+    if (isNumeric(par, include.int = TRUE)) {
+      # repair scalar non-NAs and vectors
       if (!(length(val) == 1L && is.na(val)) && any(val < par$lower | val > par$upper)) {
         if (warn) {
-          warningf("Repairing value for %s: %s", par$id, as.character(val))
+          warningf("Repairing value for %s: %s", par$id, paramValueToString(par, val))
         }
         val = pmax(par$lower, val)
         val = pmin(par$upper, val)
