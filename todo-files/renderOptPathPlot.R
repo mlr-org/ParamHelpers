23db595ed8a57235f3c600492fb14c364b643ad8
Daniel Horn <daniel.horn@tu-dortmund.de>
2015-02-18 12:56:54 +0100

    plot opt.path

--- todo-files/renderOptPathPlot.R
+++ todo-files/renderOptPathPlot.R
@@ -9,7 +9,7 @@
 #'   Activates or deactivates the alpha fading for the parallel X-space plot. Default is \code{TRUE}.
 #' @return List of plots, one for each iteration.
 #' @export
-renderOptPathPlot = function(op, iters, alpha = TRUE) {
+renderOptPathPlot = function(op, iters = NULL, alpha = TRUE) {
   requirePackages("GGally")
   requirePackages("ggplot2")
   requirePackages("gridExtra")
@@ -21,7 +21,6 @@ renderOptPathPlot = function(op, iters, alpha = TRUE) {
   if (any(iters > max(getOptPathDOB(op))))
     stop("You want to plot an iteration that does not exist!")
   
-  # FIXME: better way to get x-names?
   x.names = colnames(getOptPathX(op))
   y.names = op$y.names
   dim.x = length(x.names)
@@ -31,16 +30,19 @@ renderOptPathPlot = function(op, iters, alpha = TRUE) {
   op.y = as.data.frame(op, include.x = FALSE, include.rest = FALSE)
   dob = getOptPathDOB(op)
   
-  if (dim.x == 2) {
-    x.min = apply(op.x, 2, min)
-    x.max = apply(op.x, 2, max)
+  # Set x and y lims - we want them constant over all plots
+  if (dim.x <= 2 ) {
+    op.x.temp = op.x[dob <= max(iters), , drop = FALSE]
+    x.min = apply(op.x.temp, 2, min)
+    x.max = apply(op.x.temp, 2, max)
     x.min = x.min - 0.05 * (x.max - x.min)
     x.max = x.max + 0.05 * (x.max - x.min)
     lim.x = cbind(x.min, x.max)
   }
-  if (dim.y == 2) {
-    y.min = apply(op.y, 2, min)
-    y.max = apply(op.y, 2, max)
+  if (dim.y <= 2) {
+    op.y.temp = op.y[dob <= max(iters), , drop = FALSE]
+    y.min = apply(op.y.temp, 2, min)
+    y.max = apply(op.y.temp, 2, max)
     y.min = y.min - 0.05 * (y.max - y.min)
     y.max = y.max + 0.05 * (y.max - y.min)
     lim.y = cbind(y.min, y.max)
@@ -49,13 +51,20 @@ renderOptPathPlot = function(op, iters, alpha = TRUE) {
   plots = list()
   for (i in seq_along(iters)) {
     # get the data
-    op.x.temp = op.x[dob <= iters[i], ,drop = FALSE]
-    op.y.temp = op.y[dob <= iters[i], ,drop = FALSE]
+    op.x.temp = op.x[dob <= iters[i], , drop = FALSE]
+    op.y.temp = op.y[dob <= iters[i], , drop = FALSE]
     dob.temp = dob[dob <= iters[i]]
+    
+    # Special case: X and Y are 1D
+    if(dim.x == 1L && dim.y == 1L) {
+      plots[[i]] = plot2D(cbind(x = op.x.temp, y = op.y.temp), dob.temp,
+        names = c(x.names, y.names), space = "both", iter = iters[i], lim = rbind(lim.x, lim.y))
+      next
+    }
     
     # plot 1: x-space
     if (dim.x == 1) {
-      pl1 = plot1D(op.x.temp, dob.temp, x.names, space = "x", iter = iters[i])
+      pl1 = plot1D(op.x.temp, dob.temp, x.names, space = "x", iter = iters[i],lim = lim.x)
     }
     if (dim.x == 2) {
       pl1 = plot2D(op.x.temp, dob.temp, x.names, space = "x", iter = iters[i], lim = lim.x)
@@ -66,7 +75,7 @@ renderOptPathPlot = function(op, iters, alpha = TRUE) {
     
     # plot 2: y-space
     if (dim.y == 1) {
-      pl2 = plot1D(op.y.temp, dob.temp, y.names, space = "y", iter = iters[i])
+      pl2 = plot1D(op.y.temp, dob.temp, y.names, space = "y", iter = iters[i], lim = lim.y)
     }
     if (dim.y == 2) {
       pl2 = plot2D(op.y.temp, dob.temp, y.names, space = "y", iter = iters[i], lim = lim.y)
@@ -81,34 +90,39 @@ renderOptPathPlot = function(op, iters, alpha = TRUE) {
 }
 
 
-#' Plot method for a one-dimensional X- or Y-Space
-#'
-#' @param dob [\code{numeric}]\cr
-#'   Vector of dates of birth of the points that should be plotted.
-#' @param names [\code{character}]\cr
-#'   Vector of the names of the variables.
-#' @param space [\code{character(1)}]\cr
-#'   If the X-Space is plotted, space = "x" and if the Y-Space is plotted, space = "y".
-#' @param iter [\code{integer(1)}]\cr
-#'   Current iteration.
-#' @return A ggplot object.
-plot1D = function(op, dob, names, space = "x", iter, alpha = TRUE) {
+# Plot method for a one-dimensional X- or Y-Space
+#
+# @param dob [\code{numeric}]\cr
+#   Vector of dates of birth of the points that should be plotted.
+# @param names [\code{character}]\cr
+#   Vector of the names of the variables.
+# @param space[\code{character}]
+#   If the X-Space is plotted, space = "x" and if the Y-Space is plotted, space = "y".
+# @param iter [\code{integer(1)}]\cr
+#   Current iteration.
+# @param lim [\code{matrix}]\cr
+#  Matrix with 1 row and 2 cols. Axis limits for the density plot.
+# @return A ggplot object.
+plot1D = function(op, dob, names, space, iter, alpha = TRUE, lim) {
   if (alpha) {
     op$.alpha = if(iter == 0)
       rep(1, length(dob)) else normalize(dob, "range", range = c(1 / (iter + 1), 1))
   }
   op$type = as.factor(ifelse(dob == 0, "init", ifelse(dob == iter, "prop", "seq")))
-  if (space == "x") {
-    title = ggtitle("X-Space")
-  } else {
-    title = ggtitle("Y-Space")
+  
+  if (space == "x") {  
+    title = ggtitle("X-Space")    
+  } 
+  if (space == "y") {    
+    title = ggtitle("Y-Space")    
   }
-    
+  
   pl = ggplot(op, aes_string(x = names[1]))
   pl = pl + geom_density(colour = "black")
   pl = pl + title
   pl = pl + geom_rug(aes(colour = type, alpha = .alpha), sides = "b", size = 2,
     data = op)
+  pl = pl + xlim(lim[1, ])
   pl = pl + guides(alpha = FALSE)
   pl = pl + scale_colour_manual(values = c("red","green", "blue"))
   pl = pl + scale_alpha_continuous(range = c(1 / (iter + 1), 1))
@@ -116,36 +130,49 @@ plot1D = function(op, dob, names, space = "x", iter, alpha = TRUE) {
 return(pl)
 }
 
-#' Plot method for a two-dimensional X- or Y-Space
-#'
-#' @param lim [\code{matrix}]\cr
-#'  Matrix with two columns. Each row contains the lower and upper limit for the 
-#'  x- and y- axes of the two-dimensional plot.
-#' @return A ggplot object.
-plot2D = function(op, dob, names, space = "x", iter, lim) {
+# Plot method for a two-dimensional X- or Y-Space
+#
+# @param lim [\code{matrix}]\cr
+#  Matrix with two columns. Each row contains the lower and upper limit for the 
+#  x- and y- axes of the two-dimensional plot.
+# @return A ggplot object.
+plot2D = function(op, dob, names, space, iter, alpha = TRUE, lim) {
+  if (alpha) {
+    op$.alpha = if(iter == 0)
+      rep(1, length(dob)) else normalize(dob, "range", range = c(1 / (iter + 1), 1))
+  }
+  
   op$type = as.factor(ifelse(dob == 0, "init", ifelse(dob == iter, "prop", "seq")))
   if (space == "x") {
     title = ggtitle("X-Space")
     x.lab = xlab(expression(x[1]))
     y.lab = ylab(expression(x[2]))
-  } else {
+  } 
+  if (space == "y") {
     title = ggtitle("Y-Space")
     x.lab = xlab(expression(y[1]))
     y.lab = ylab(expression(y[2]))
   }
+  if (space == "both") {
+    title = ggtitle("X- and Y-Space")
+    x.lab = xlab(expression(x[1]))
+    y.lab = ylab(expression(y[1]))
+  }
   
-  pl = ggplot(op, aes_string(x = names[1], y = names[2], shape = "type", colour = "type"))
+  pl = ggplot(op, aes_string(x = names[1], y = names[2], shape = "type",
+    colour = "type", alpha = ".alpha"))
   pl = pl + geom_point(size = 3)
   pl = pl + title
   pl = pl + x.lab + y.lab
   pl = pl + xlim(lim[1, ]) + ylim(lim[2,])
   pl = pl + scale_colour_manual(values = c("red","green", "blue"))
+  pl = pl + scale_alpha_continuous(range = c(1 / (iter + 1), 1))
   pl = pl + theme(legend.position = "top")
   return(pl)
 }
 
-#' Plot method for a multi-dimensional X- or Y-Space
-#' @return A ggplot object.
+# Plot method for a multi-dimensional X- or Y-Space
+# @return A ggplot object.
 plotMultiD = function(op, dob, names, space = "x", iter, alpha = TRUE) {
   args = list(columns = seq_along(names))
   if (alpha) {
--- todo-files/test_renderOptPathPlot.R
+++ todo-files/test_renderOptPathPlot.R
@@ -1,6 +1,22 @@
 context("renderOptPathPlot")
 
 test_that("renderOptPathPlot", {
+  # 1D-1D
+  ps0 = makeParamSet(
+    makeNumericParam("x")
+  )
+  op0 = makeOptPathDF(par.set = ps0, y.names = c("y"), minimize = TRUE)
+  X = rnorm(20)
+  dob = c(rep(0, 5), 1:5)
+  for (i in 1:10) {
+    addOptPathEl(op0, x = list(
+      x = X[i * 2]),
+      y = X[ i * 2 - 1],
+      dob = dob[i])
+  }
+  pl <- renderOptPathPlot(op0, iters = 1:5)
+  
+  
   # 2D-2D
   ps1 = makeParamSet(
     makeNumericParam("x"),
@@ -10,19 +26,24 @@ test_that("renderOptPathPlot", {
   X = rnorm(40)
   dob = c(rep(0, 5), 1:5)
   for (i in 1:10) {
-    addOptPathEl(op1, x = list(x = X[i*4-3], z = X[i*4-2]), y = c(y1 = X[i*4-1], y2 = X[i*4]), dob = dob[i])
+    addOptPathEl(op1, x = list(
+      x = X[i * 4 - 3], z = X[i * 4 - 2]),
+      y = c(y1 = X[ i * 4 - 1], y2 = X[i * 4]),
+      dob = dob[i])
   }
-  renderOptPathPlot(op1, iters = 1:5)
+  pl <- renderOptPathPlot(op1, iters = 1:5)
+  
+  
   
   # 2D-3D
   op2 = makeOptPathDF(par.set = ps1, y.names = c("y1", "y2", "y3"), minimize = c(TRUE, FALSE, TRUE))
   X = rnorm(50)
   dob = c(rep(0, 5), 1:5)
   for (i in 1:10) {
-    addOptPathEl(op2, x = list(x = X[i*5-4], z = X[i*5-3]) , y = c(y1 = X[i*5-2], 
-      y2 = X[i*5-1], y3 = X[i*5]), dob = dob[i])
+    addOptPathEl(op2, x = list(x = X[i * 5 - 4], z = X[i * 5 - 3]),
+      y = c(y1 = X[i * 5 - 2], y2 = X[i * 5 - 1], y3 = X[i * 5]), dob = dob[i])
   }
-  renderOptPathPlot(op2, iters = 1:5)
+  pl <- renderOptPathPlot(op2, iters = 1:5)
   
   # 3D-3D
   ps3 = makeParamSet(
@@ -34,10 +55,10 @@ test_that("renderOptPathPlot", {
   X = rnorm(60)
   dob = c(rep(0, 5), 1:5)
   for (i in 1:10) {
-    addOptPathEl(op3, x = list(x = X[i*6-5], z = X[i*6-4], z2 = X[i*6-3]) , 
-      y = c(y1 = X[i*6-2], y2 = X[i*6-1], y3 = X[i*6]), dob = dob[i])
+    addOptPathEl(op3, x = list(x = X[i * 6 - 5], z = X[i * 6 - 4], z2 = X[i * 6 - 3]) , 
+      y = c(y1 = X[i * 6 - 2], y2 = X[i * 6 - 1], y3 = X[i * 6]), dob = dob[i])
   }
-  renderOptPathPlot(op3, iters = 1:5)
+  pl <- renderOptPathPlot(op3, iters = 1:5)
   
   # 3D-1D
   ps4 = makeParamSet(
@@ -49,10 +70,10 @@ test_that("renderOptPathPlot", {
   X = rnorm(40)
   dob = c(rep(0, 5), 1:5)
   for (i in 1:10) {
-    addOptPathEl(op4, x = list(x = X[i*4-3], z = X[i*4-2], z2 = X[i*4-1]) , 
-                 y = c(y1 = X[i*4]), dob = dob[i])
+    addOptPathEl(op4, x = list(x = X[i * 4 - 3], z = X[i * 4 - 2], z2 = X[i * 4 - 1]), 
+                 y = c(y1 = X[i * 4]), dob = dob[i])
   }
-  renderOptPathPlot(op4, iters = 1:5)
+  pl <- renderOptPathPlot(op4, iters = 1:5)
   
 })
   
