d32fb949fa48cb34df537fd749adf857d41e4bae
Jakob Richter <code@jakob-r.de>
2015-06-23 21:29:35 +0200

    small fix in documentation

--- R/isRequiresOk.R
+++ R/isRequiresOk.R
@@ -9,8 +9,9 @@
 #'   List of parameter settings.
 #' @param ids \code{character()} \cr
 #'   \code{id}s of the param.vals to check. Default is \code{names(par.vals)}.
-#' @return \code{logical(1)} \cr
+#' @param use.defaults \code{logical()} \cr
 #'   Some requirements relay on default values of the \code{par.set}. Default is \code{TRUE}, which means that if the value is not present in \code{par.vals} the default value will be considered.
+#' @return \code{logical(1)} \cr
 #' @export
 isRequiresOk = function(par.set, par.vals, ids = names(par.vals), use.defaults = TRUE) {
   assertClass(par.set, "ParamSet")
