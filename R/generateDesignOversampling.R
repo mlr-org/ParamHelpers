b6743631bb6f83621973513d6259317c6992793e
Daniel Horn <daniel.horn@tu-dortmund.de>
2016-04-05 13:30:52 +0200

    Finished new generate design method + unit test

--- R/generateDesignOversampling.R
+++ R/generateDesignOversampling.R
@@ -1,17 +1,42 @@
-library(cluster)
+#' @title Generates a random design for a mixed parameter set using oversampling
+#'        and removing "close" design points. 
+#'
+#' @description
+#' The function simply calls \code{\link{generateRandomDesign}} generating
+#' \code{oversampling} more points than desired. Afterwards, repeatedly one of
+#' the 2 points with the smallest Gower distance is removed until the design
+#' is reduced to size \code{n}.
+#'
+#' @template arg_gendes_n
+#' @template arg_parset
+#' @template arg_trafo
+#' @param oversampling [\code{double}]\cr
+#'   Oversampling ratio. Must be greater than 1.
+#' @template ret_gendes_df
+# Don't export yet!
+# @export
 
 generateDesignOversampling = function(
   n = 10L, par.set, trafo = FALSE, oversampling = 20L) {
   
-  design = convertListOfRowsToDataFrame(sampleValues(par.set, oversampling * n,
-    trafo = trafo))
-  dists = daisy(x = design, metric = "gower")
+  requirePackages("cluster", why = "generateDesignOversampling")
   
-  # I know, we double the memory requirement here, but working with a matrix
-  # is much much easier ...
+  n = asCount(n)
+  doBasicGenDesignChecks(par.set)
+  assertNumber(oversampling, lower = 1L)
+  assertLogical(trafo, len = 1L)
+  
+  # Sample random Design with to many observations
+  design = generateRandomDesign(n * oversampling, par.set, trafo)
+  
+  # Calculate dist matrix. I know, we double the memory requirement here, 
+  # but working with a matrix is much much easier ... we probably want to do
+  # this in C on the long hand!
+  dists = daisy(x = design, metric = "gower")
   dists = as.matrix(dists)
   diag(dists) = Inf
   
+  # Shrink the design to n observations
   while (nrow(design) > n) {
     # Which observations should be removed? Sample between the candidates
     remove.ind = sample(which(apply(dists == min(dists), 1, any)), 1)
@@ -22,27 +47,3 @@ generateDesignOversampling = function(
   
   return(design)
 }
-
-
-
-par.set =   makeParamSet(
-  makeDiscreteParam("selected.learner", values = c("a", "b", "c")),
-  
-  makeIntegerParam("classif.randomForest.mtry",
-    lower = 15, upper = 45,
-    requires = quote(selected.learner == "a")),
-  makeIntegerParam("classif.randomForest.nodesize", lower = 1L, upper = 120,
-    requires = quote(selected.learner == "a")),
-  
-  makeNumericParam("classif.svm.cost", lower = -15L, upper = 15L, trafo = function(x) 2^x,
-    requires = quote(selected.learner == "b")),
-  makeDiscreteParam("classif.svm.kernel", values = c("linear", "radial"),
-    requires = quote(selected.learner == "b")),
-  makeNumericParam("classif.svm.gamma", lower = -15L, upper = 15L, trafo = function(x) 2^x,
-    requires = quote(selected.learner == "b" & classif.svm.kernel == "radial")),
-  
-  makeIntegerParam("classif.kknn.k", lower = 1L, upper = 21L,
-    requires = quote(selected.learner == "c"))
-)
-
-des = generateDesignOversampling(20, par.set)
