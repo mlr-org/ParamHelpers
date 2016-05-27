44dcddc28910501622152dac4699de9269059b3d
Daniel Horn <daniel.horn@tu-dortmund.de>
2016-05-27 12:20:28 +0200

    generateDesignOversampling can now extend a start.design

--- R/generateDesignOversampling.R
+++ R/generateDesignOversampling.R
@@ -12,12 +12,19 @@
 #' @template arg_trafo
 #' @param oversampling [\code{double}]\cr
 #'   Oversampling ratio. Must be greater than 1.
+#' @param start.design [\code{data.frame}]
+#'   The start design will be extended until size \code{n} is reached.
+#'   The first rows of the generated design will match the start.design.
+#'   Default is an empty data.frame.
 #' @template ret_gendes_df
 # Don't export yet!
 # @export
 
 generateDesignOversampling = function(
-  n = 10L, par.set, trafo = FALSE, oversampling = 20L) {
+  n = 10L, par.set, trafo = FALSE, oversampling = 20L,
+  # FIXME: is there a better way to create the empty data.frame?
+  # makeDataFrame in BBmisc does not work with factors ...
+  start.design = generateDesign(1, par.set)[NULL, ]) {
   
   requirePackages("cluster", why = "generateDesignOversampling")
   
@@ -25,21 +32,43 @@ generateDesignOversampling = function(
   doBasicGenDesignChecks(par.set)
   assertNumber(oversampling, lower = 1L)
   assertLogical(trafo, len = 1L)
+  assertDataFrame(start.design, types = getParamTypes(par.set, df.cols = TRUE),
+    ncols = sum(getParamLengths(par.set)))
   
+  # FIXME: Do we need a helper here?
+  # I want to check that every row of a design is a feasible param.
+  feas = sapply(
+    convertRowsToList(start.design, name.list = TRUE, name.vector = TRUE),
+    isFeasible, par = par.set)
+  if (!all(feas)) {
+    stop("One row of the start.design is not feasible.")
+  }
+
   # Sample random Design with to many observations
   design = generateRandomDesign(n * oversampling, par.set, trafo)
+  
+  # Add start.desing
+  design = rbind(start.design, design)
   
   # Calculate dist matrix. I know, we double the memory requirement here, 
   # but working with a matrix is much much easier ... we probably want to do
   # this in C on the long hand!
   dists = cluster::daisy(x = design, metric = "gower")
   dists = as.matrix(dists)
+  # diag = Inf, not 0
   diag(dists) = Inf
+  # start.design points are fixed
+  dists[seq_row(start.design), seq_row(start.design)] = Inf
   
   # Shrink the design to n observations
   while (nrow(design) > n) {
     # Which observations should be removed? Sample between the candidates
-    remove.ind = sample(which(apply(dists == min(dists), 1, any)), 1)
+    remove.ind = which(apply(dists == min(dists), 1, any))
+    # Never remove start.design points
+    remove.ind = remove.ind[remove.ind > nrow(start.design)]
+    # If multiple candidates, sample
+    if (length(remove.ind) > 1)
+      remove.ind = sample(remove.ind, 1)
     # Remove it
     design = design[-remove.ind, ]
     dists = dists[-remove.ind, -remove.ind]
--- tests/testthat/test_generateDesignOversampling.R
+++ tests/testthat/test_generateDesignOversampling.R
@@ -26,6 +26,7 @@ test_that("generateDesignOversampling", {
   
   des = generateDesignOversampling(13L, par.set, oversampling = 10L)
   
+  # A how lot of tests ...
   expect_equal(nrow(des), 13L)
   expect_equal(ncol(des), 7L)
   expect_equal(colnames(des), paste("x", 1:7, sep = ""))
@@ -37,16 +38,25 @@ test_that("generateDesignOversampling", {
   expect_true(is.numeric(des[, 6L]))
   expect_true(is.integer(des[, 7L]))
   expect_true(all(des[, 1L] %in% c("a", "b", "c")))
-  expect_true(is.na(des[, 2L]) || des[, 2L] >= 15 && des[, 2L] <= 45)
-  expect_true(des[, 1L] == "a" || is.na(des[, 2L]))
-  expect_true(is.na(des[, 3L]) || des[, 3L] >= 1 && des[, 3L] <= 120)
-  expect_true(des[, 1L] == "a" || is.na(des[, 3L]))
-  expect_true(is.na(des[, 4L]) || des[, 4L] >= -15 && des[, 4L] <= 15)
-  expect_true(des[, 1L] == "b" || is.na(des[, 4L]))
-  expect_true(is.na(des[, 5L]) || des[, 5L] %in% c("linear", "radial"))
-  expect_true(des[, 1L] == "b" || is.na(des[, 5L]))
-  expect_true(is.na(des[, 6L]) || des[, 6L] >= -15 && des[, 6L] <= 15)
-  expect_true(des[, 1L] == "b" && des[, 5L] == "radial" || is.na(des[, 5L]))
-  expect_true(is.na(des[, 7L]) || des[, 7L] >= 1 && des[, 7L] <= 21)
-  expect_true(des[, 1L] == "c" || is.na(des[, 7L]))
+  expect_true(all(is.na(des[, 2L]) | des[, 2L] >= 15 & des[, 2L] <= 45))
+  expect_true(all(des[, 1L] == "a" | is.na(des[, 2L])))
+  expect_true(all(is.na(des[, 3L]) | des[, 3L] >= 1 & des[, 3L] <= 120))
+  expect_true(all(des[, 1L] == "a" | is.na(des[, 3L])))
+  expect_true(all(is.na(des[, 4L]) | des[, 4L] >= -15 & des[, 4L] <= 15))
+  expect_true(all(des[, 1L] == "b" | is.na(des[, 4L])))
+  expect_true(all(is.na(des[, 5L]) | des[, 5L] %in% c("linear", "radial")))
+  expect_true(all(des[, 1L] == "b" | is.na(des[, 5L])))
+  expect_true(all(is.na(des[, 6L]) | des[, 6L] >= -15 & des[, 6L] <= 15))
+  expect_true(all(des[, 1L] == "b" & des[, 5L] == "radial" | is.na(des[, 6L])))
+  expect_true(all(is.na(des[, 7L]) | des[, 7L] >= 1 & des[, 7L] <= 21))
+  expect_true(all(des[, 1L] == "c" | is.na(des[, 7L])))
+  
+  
+  # Does start.design work?
+  start.design = generateDesign(2L, par.set)
+  des = generateDesignOversampling(13L, par.set, oversampling = 10L, start.design = start.design)
+  
+  expect_equal(des[1, ], start.design[1, ])
+  expect_equal(des[2, ], start.design[2, ])
+  
 })
