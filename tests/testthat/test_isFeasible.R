14a39e1b7864aeae9161e65081a5e8a6bf5e987a
Pascal Kerschke <pascal.kerschke@uni-muenster.de>
2016-08-10 00:53:12 +0200

    solved fixme in isFeasible-test

--- tests/testthat/test_isFeasible.R
+++ tests/testthat/test_isFeasible.R
@@ -30,6 +30,28 @@ test_that("isFeasible LearnerParamSet", {
     makeIntegerLearnerParam("a", default = 1L),
     makeDiscreteLearnerParam("b", values = list('0' = "NIL", '1' = "ONE", 'f' = function(x) x + 1)),
     makeIntegerLearnerParam("c", default = 1L, requires = quote(a == 1)),
+    makeIntegerLearnerParam("d", default = 1L, requires = quote(a == 2))
+  )
+
+  expect_true(isFeasible(ps, list(a = 1, b = function(x) x+1, c = 1, d = NA)))
+  expect_true(isFeasible(ps, list(a = 1, b = function(x) x+1, c = 1), filter = TRUE))
+  expect_true(isFeasible(ps, list(a = 2, b = function(x) x+1, d = 1), filter = TRUE))
+  expect_error(isFeasible(ps, list(a = 1, c = 1)), "does not match ParamSet length")
+  expect_error(isFeasible(ps, list(1, 1)), "does not match ParamSet length")
+  expect_error(isFeasible(ps, list(1, 1), filter = TRUE), "only works with named input")
+  expect_error(isFeasible(ps, list(a = 1, zzz = 1), filter = TRUE), "zzz")
+  expect_false((res = isFeasible(ps, list(a = 1, c = "1"), filter = TRUE)))
+  expect_match(attr(res, "warning"), "c=1 does not meet constraints")
+  expect_error(isFeasible(ps, list(c = 2), filter = TRUE), "needed for requirements: a")
+  expect_false((res = isFeasible(ps, list(a = 2, c = 2), filter = TRUE)))
+  expect_match(attr(res, "warning"), "Param c=2 is set but does not meet requirements")
+  expect_true((res = isFeasible(ps, list(a = 2, c = NA), filter = TRUE)))
+  expect_true(isFeasible(ps, list(c = 2), filter = TRUE, use.defaults = TRUE))
+
+  ps = makeParamSet(
+    makeIntegerLearnerParam("a", default = 1L),
+    makeDiscreteLearnerParam("b", values = list('0' = "NIL", '1' = "ONE", 'f' = function(x) x + 1)),
+    makeIntegerLearnerParam("c", default = 1L, requires = quote(a == 1)),
     makeIntegerLearnerParam("d", default = 1L, requires = quote(a == 2)),
     makeIntegerLearnerParam("e", default = expression(n), upper = expression(2*n)),
     keys = "n"
@@ -48,9 +70,6 @@ test_that("isFeasible LearnerParamSet", {
   expect_false((res = isFeasible(ps, list(a = 2, c = 2), filter = TRUE)))
   expect_match(attr(res, "warning"), "Param c=2 is set but does not meet requirements")
   expect_true((res = isFeasible(ps, list(a = 2, c = NA), filter = TRUE)))
-  # FIXME: if we want to use defaults and the ps contains expressions, then we need
-  # to pass a dictionary
-  # expect_true(isFeasible(ps, list(c = 2), filter = TRUE, use.defaults = TRUE))
 })
 
 test_that("length of vectors", {
@@ -62,5 +81,6 @@ test_that("length of vectors", {
   )
   
   expect_true(isFeasible(ps, list(a = 1, b = 2, c = rep(3, 4))))
-  expect_false(isFeasible(ps, list(a = 1, b = 2, c = 3)))
+  expect_false((res = isFeasible(ps, list(a = 1, b = 2, c = 3))))
+  expect_match(attr(res, "warning"), "The parameter setting c=3 does not meet constraints")
 })
