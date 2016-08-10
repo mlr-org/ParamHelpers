5cd39d6233ede164cf3c5f3f7c7f1dad397996f5
Jakob Richter <code@jakob-r.de>
2016-08-10 16:12:19 +0200

    fix: updateParVals also respects defaults to not drop old.par.vals

--- tests/testthat/test_updateParVals.R
+++ tests/testthat/test_updateParVals.R
@@ -2,15 +2,17 @@ context("updateParVals")
 
 test_that("updateParVals works", {
   #simple dependency
-  pa = list(a = 1, b = 2, d = 4)
+  pa = list(a = 1, b = 2, d = 4, e = 5)
   pb = list(a = 0, c = 3)
   ps = makeParamSet(
     makeIntegerParam("a"),
     makeIntegerParam("b", requires = quote(a == 1)),
     makeIntegerParam("c"),
-    makeIntegerParam("d"))
+    makeIntegerParam("d"),
+    makeIntegerParam("e", requires = quote(f == TRUE)),
+    makeLogicalParam("f", default = TRUE))
   pc = updateParVals(ps, pa, pb)
-  expect_equal(pc, list(a = 0, c = 3, d = 4))
+  expect_equal(pc, list(a = 0, c = 3, d = 4, e = 5))
   expect_warning(updateParVals(ps, pa, pb, warn = TRUE), "ParamSetting b=2")
 
   #sanity
@@ -21,10 +23,11 @@ test_that("updateParVals works", {
   pa = list(a = 1, b = 2, c = 0.5, d = 4)
   pb = list(a = -3)
   ps = makeParamSet(
-    makeIntegerParam("a"),
+    makeIntegerParam("a", requires = quote(e == TRUE)),
     makeIntegerParam("b", requires = quote(a == 1)),
     makeNumericParam("c"),
-    makeIntegerParam("d", requires = quote(b == 2)))
+    makeIntegerParam("d", requires = quote(b == 2)),
+    makeLogicalParam("e", default = TRUE))
   pc = updateParVals(ps, pa, pb)
   expect_equal(pc, list(a = -3, c = 0.5))
 })
