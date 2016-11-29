fb36319ac1e93eb4be9c7db3413dec850e24e159
Bernd Bischl <bernd_bischl@gmx.net>
2016-11-29 19:48:31 +0100

    ...

--- R/getRequirements.R
+++ R/getRequirements.R
@@ -4,19 +4,26 @@
 #' Returns all \code{require}s-objects of a param set as a list.
 #'
 #' @template arg_parset
-#' @param remove.null [\code{logical(1)}]\cr
-#'   If not set, params without a requires-setting will result in a \code{NULL} element in the returned list,
-#'   otherwise they are removed.
-#'   Default is code{TRUE}.
+#' @param no.conditions [any]\cr
+#'   Params without a requires-setting will result in this value in the returned list.
+#'   Default is \dQuote"remove} which means to remove them from the list.
 #' @return [named \code{list}].
 #'   Named list of require-call-objects, lengths corresponds to number of params (potentially
 #'   only the subset with requires-field), named with with param ids.
 #' @export
-getRequirements = function(par.set, remove.null = TRUE) {
+getRequirements = function(par.set, no.conditions = "remove") {
   assertClass(par.set, "ParamSet")
-  assertFlag(remove.null)
-  res = extractSubList(par.set$pars, "requires", simplify = FALSE, use.names = TRUE)
-  if (remove.null)
+  nocval = no.conditions
+  if (identical(no.conditions, "remove"))
+    nocval = NULL
+  res = lapply(par.set$pars, function(p) {
+    if (is.null(p$requires))
+      return(nocval)
+    else
+      return(p$requires)
+  })
+  names(res) = names(par.set$pars)
+  if (identical(no.conditions, "remove"))
     res = filterNull(res)
   return(res)
 }
--- tests/testthat/test_generateDesignOfDefaults.R
+++ tests/testthat/test_generateDesignOfDefaults.R
@@ -1,62 +1,62 @@
-context("generateDefaultDesign")
+context("generateDesignOfDefaults")
 
 
-test_that("generateDefaultDesign", {
+test_that("generateDesignOfDefaults", {
   ps = makeParamSet(
     makeNumericParam("x", lower = 1, upper = 5, default = 1),
     makeIntegerParam("y", lower = 2, upper= 6, default = 3)
   )
-  d = generateDefaultDesign(ps)
+  d = generateDesignOfDefaults(ps)
   e = data.frame(x = 1, y = 3)
   attr(e, "trafo") = FALSE
   expect_equal(d, e)
-  
+
   ps = makeParamSet(
     makeNumericParam("u", lower = 1, upper = 5, default = 1),
     makeIntegerParam("v", lower = 2, upper= 6, default = 3),
     makeLogicalParam("w", default = TRUE),
     makeDiscreteParam("x", values = c("a", "b"), default = "a")
   )
-  d = generateDefaultDesign(ps)
+  d = generateDesignOfDefaults(ps)
   e = data.frame(u = 1, v = 3, w = TRUE, x = factor("a", levels = c("a", "b")))
   attr(e, "trafo") = FALSE
   expect_equal(d, e)
-  
+
   # vectors
   ps = makeParamSet(
     makeNumericVectorParam("x", len = 2L, lower = 1, upper = 2, default = c(1, 2)),
     makeIntegerVectorParam("y", len = 2L, lower = 3, upper = 4, default = c(3,3)),
     makeLogicalVectorParam("z", len = 2L, default = c(TRUE, FALSE))
   )
-  d = generateDefaultDesign(ps)
+  d = generateDesignOfDefaults(ps)
   e = data.frame(x1 = 1, x2 = 2, y1 = 3, y2 = 3, z1 = TRUE, z2 = FALSE)
   attr(e, "trafo") = FALSE
   expect_equal(d, e)
-  
+
   # trafo
   ps = makeParamSet(
     makeNumericParam("x", lower = 0, upper = 1, default = 0),
     makeNumericParam("y", lower = 3, upper = 4, trafo = function(x) 2*x, default = 3)
   )
-  d = generateDefaultDesign(ps, trafo = TRUE)
+  d = generateDesignOfDefaults(ps, trafo = TRUE)
   e = data.frame(x = 0, y = 6)
   attr(e, "trafo") = TRUE
   expect_equal(d, e)
-  
+
   # missing default
   ps = makeParamSet(
     makeNumericParam("x", lower = 1, upper = 5),
     makeIntegerParam("y", lower = 2, upper= 6, default = 3)
   )
-  expect_error(generateDefaultDesign(ps), regexp = "*No default parameter setting for x")
-  
+  expect_error(generateDesignOfDefaults(ps), regexp = "No default parameter setting for: x")
+
   # dependent parameter spaces
   ps = makeParamSet(
     makeNumericParam("x", lower = 1, upper = 3, default = 2),
     makeNumericParam("y", lower = 1, upper = 3, default = 1, requires = quote(x > 2))
   )
-  d = generateDefaultDesign(ps)
-  e = data.frame(x = 2, y = NA)
+  d = generateDesignOfDefaults(ps)
+  e = data.frame(x = 2, y = NA_real_)
   attr(e, "trafo") = FALSE
   expect_equal(d, e)
 })
