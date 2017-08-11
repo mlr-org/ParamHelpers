7914e3512d50caead14ed2947f49c5423f47c317
Jakob Richter <code@jakob-r.de>
2017-08-11 15:18:17 +0200

    fix naming and documentation

--- tests/testthat/test_isSimpleNumeric.R
+++ tests/testthat/test_isSimpleNumeric.R
@@ -1,22 +1,22 @@
-context("isSimpleNumeric")
+context("isNumericNoRequires")
 
-test_that("isSimpleNumeric", {
+test_that("isNumericNoRequires", {
   ps = makeParamSet()
-  expect_true(isSimpleNumeric(ps))
+  expect_true(isNumericNoRequires(ps))
   
   ps = makeParamSet(
     makeNumericParam("x", lower=1, upper=2)
   )
-  expect_true(isSimpleNumeric(ps))
+  expect_true(isNumericNoRequires(ps))
   
   ps = makeParamSet(
     makeDiscreteParam("x", values=1:2)
   )
-  expect_false(isSimpleNumeric(ps))
+  expect_false(isNumericNoRequires(ps))
   
   ps = makeParamSet(
     makeDiscreteParam("x", values=1:2),
     makeNumericParam("y", lower=1, upper=2, requires=quote(x == 1))
   )
-  expect_false(isSimpleNumeric(ps))
+  expect_false(isNumericNoRequires(ps))
 })
