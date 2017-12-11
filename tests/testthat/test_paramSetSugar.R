7e3e20926157b680ea2cb352b0afbd1b25a4fe72
mb706 <mb706@uni>
2017-12-11 20:58:08 +0100

    a few more tests

--- tests/testthat/test_paramSetSugar.R
+++ tests/testthat/test_paramSetSugar.R
@@ -9,8 +9,12 @@ test_that("ParamSetSugar generates the expected ParamSets", {
 
   # list of pairs that whould have identical elements
   param.sets = list(
-      list(makeParamSet(makeIntegerLearnerParam("a"), makeIntegerLearnerParam("b")),
-        pSSLrn(a: integer[, ], b = NA: integer[, ])),
+
+      # empty defaults
+      list(makeParamSet(makeIntegerLearnerParam("a"), makeIntegerLearnerParam("b"), makeIntegerLearnerParam("c")),
+        pSSLrn(a: integer[, ], b = NA: integer[, ], c: integer)),
+
+      # discrete parameters
       list(makeParamSet(makeDiscreteLearnerParam("b", c("a", "b", "c")),
         makeDiscreteLearnerParam("c", list(a = 1, b = 2, c = 3), default = 1),
         makeDiscreteVectorLearnerParam("d", 2, list(a = "a", b = "b", `1` = 1), default = list("a", 1)),
@@ -19,16 +23,21 @@ test_that("ParamSetSugar generates the expected ParamSets", {
           c = 1: discrete[a = 1, b = 2, c = 3],
           d = list("a", 1): discrete[a, b, 1]^2,
           e = "b": discrete[param.list] [[when = "both", requires = expression(b == "a")]])),
+
+      # ranges, extra parameters
       list(makeParamSet(makeNumericParam("a", allow.inf = TRUE, default = 1, tunable = FALSE),
         makeNumericParam("b", lower = 0, allow.inf = TRUE, default = 2),
         makeNumericVectorParam("c", 3, upper = 0, allow.inf = FALSE, special.vals = list(-1)),
         makeNumericVectorParam("d", 2, lower = 0, upper = 1, default = c(0.5, 0.5), requires = expression(a == 0))),
-        pSSLrn(a = 1: numeric[, ] [[tunable = FALSE]],
+        pSSLrn(a = 1: numeric [[tunable = FALSE]],
           b = 2: numeric[0, ],
           c: numeric[~., x - 10] ^ (2 + 1) [[special.vals = list(1 - 2)]],
           d = c(1 / 2, 0.5): numeric[0, 1]^2 [[requires = expression(a == 0), allow.inf = FALSE]], .pss.learner.params = FALSE)),
+
+      # empty parameter set
       list(makeParamSet(),
         pSSLrn())
+
   )
 
   for (ps.pair in param.sets) {
@@ -39,12 +48,15 @@ test_that("ParamSetSugar generates the expected ParamSets", {
 
 test_that("ParamSetSugar works with params that have no bounds", {
 
-  ps.sugar = pSS(a = TRUE: logical, b = identity: funct, c = "test": character, d = list(1, 2, 3): untyped)
+  ps.sugar = pSS(a = TRUE: logical, b = identity: funct, c = "test": character, d = list(1, 2, 3): untyped, e = 1: numeric, f = 0: integer)
+
   ps.normal = makeParamSet(
       makeLogicalParam("a", TRUE),
       makeFunctionParam("b", identity),
       makeCharacterParam("c", "test"),
-      makeUntypedParam("d", list(1, 2, 3)))
+      makeUntypedParam("d", list(1, 2, 3)),
+      makeNumericParam("e", default = 1, allow.inf = TRUE),
+      makeIntegerParam("f", default = 0))
 
   expect_identical(ps.sugar, ps.normal)
 
@@ -116,5 +128,32 @@ test_that("ParamSetSugar works when called indirectly", {
   x = 1
   expect_identical(pss1(.pss.learner.params = FALSE, a = x: integer[x, x * 2]^x [[tunable = (x == 2)]]),
     makeParamSet(makeIntegerVectorParam("a", 1, 1, 2, default = 1, tunable = FALSE)))
+})
+
+test_that("ParamSetSugar gives error messages", {
+
+  expect_error(pSS(test), ":` was missing")
+  expect_error(pSS(1 + 1), ":` was missing")
+  expect_error(pSS("a: integer"), ":` was missing")
+
+  expect_error(pSS(x: function(){}), "Unknown parameter type function")
+
+  expect_error(pSSLrn(x: character), "'character' not allowed for Learner params.")
+  expect_identical(pSS(x: character), makeParamSet(makeCharacterParam("x")))
+
+  expect_error(pSS(x: nonexistent), "Unknown parameter type nonexistent")
+
+  expect_error(pSS(x: funct[1, 2]), "'funct' parameter may not have range parameter")
+
+  expect_error(pSS(x: discrete), "discrete parameter must be of the form.*Range suffix.*is missing")
+
+  expect_error(pSS(x: funct ^ list), "`\\^` found, but exponent list did not eval to numeric")
+
+  expect_error(pSS(x: discrete[1, 2, 1 + 1]), "value list .*1 \\+ 1) invalid")
+
+  expect_error(pSS(x: numeric[1, 2, 3]), "invalid numeric / integer range")
+
+  expect_error(pSS(x: integer[~., ]), '"~."-bounds \\(unbounded but excluding "Inf"\\) are only allowed for "numeric" variables.')
+
 })
 
